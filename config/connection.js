024b49dff952b9aa795d1a9fc3f6692814d7c3f7
Mariana <garman.mariana@gmail.com>
2019-10-29 17:15:57 -0700

    added config folder

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+//Create the MySQL connection object
+var connection = mysql.createConnection({
+    //DB is local on localhost
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+// Make connection to MySQL
+connection.connect(function(err) {
+    if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for ORM to use.
+module.exports = connection;
